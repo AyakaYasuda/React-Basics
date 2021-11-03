b5d7f1644df4e0900453221864a59e10bf41d3e4
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-11-03 00:43:28 -0700

    added state to update titles

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = props => {
+  const [title, setTitle] = useState(props.title);
+  // it's so called React Hook and muct be called directly inside the function
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
